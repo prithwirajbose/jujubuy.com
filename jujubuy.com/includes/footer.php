244e95fd32aa81d14c84058d4503b2e6d72d9425
Prithwiraj Bose <sribasu@gmail.com>
2017-03-21 02:46:34 +0530

    Grid Layout Template creation

--- jujubuy.com/includes/footer.php
+++ jujubuy.com/includes/footer.php
@@ -1,3 +1,3 @@
-<div class="col3 rightseparator"><a href="template1.php">Template 1</a></div>
-<div class="col3 rightseparator">Here goes link 2</div>
+<div class="col3 rightseparator"><a href="template1.php">Widget Template</a></div>
+<div class="col3 rightseparator"><a href="template2.php">Grid Template</a></div>
 <div class="col3">Here goes link 3</div>
---
+++ jujubuy.com/template2.php
@@ -0,0 +1,54 @@
+<?php include("config.php"); ?>
+<?php include("functions.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Buy Online Electronics Components, IoT Modules, Robotics, Project Parts, Integrated Circuits, Microcontrollers and Semi-Conductors | JujuBuy.com</title>
+<meta name="description" content="Buy Online Electronic Components, Parts, Modules, IoT Components, Robotics, Integrated Circuits, Microcontrollers, SemiConductors, Transistor, Passive Components, Resistor, Capacitor, Diode, Inductor" />
+<meta name="keyword" content="electronic,components,parts,modules,iot,robotics,integrated circuits,ic,microcontroller,semiConductor,transistor,resistor,capacitor,diode,inductor,project material,enclosure,wire,relay,sensors" />
+<?php include("includes/head.php"); ?>
+</head>
+<body>
+<!-- Begin Wrapper -->
+<div id="wrapper">
+<!-- Begin Header -->
+<div id="header">
+<?php include("includes/header.php"); ?>
+</div>
+<!-- End Header -->
+<!-- Begin Naviagtion -->
+<div id="navigation">
+<?php include("includes/navmenu.php"); ?>
+<?php include("includes/usermenu.php"); ?>
+<div class="clear"></div>
+</div>
+<!-- End Naviagtion -->
+<!-- Begin Content -->
+<div id="content">
+
+
+
+
+<div class="gridcontainer latest-products">
+<h2 class="section-title small">Latest Products</h2>
+<?php for($xxx=0;$xxx<8;$xxx++) { ?>
+<div class="griditem">
+
+</div>
+<?php }
+?>
+</div>
+
+
+
+
+</div>
+<!-- End Content -->
+<div id="footer">
+<?php include("includes/footer.php"); ?>
+<div class="clear"></div>
+</div>
+</div>
+<!-- End Wrapper -->
+</body>
+</html>
